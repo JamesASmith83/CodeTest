5c167b46b6d3a9c45cadb11729d5229b81056703
James Smith <jamessmith@jamess-air.home>
2021-08-31 19:59:19 +0100

    routes configured and tests

--- Test/Integration/test-route-users-active.js
+++ Test/Integration/test-route-users-active.js
@@ -4,3 +4,11 @@ app = require('../../app');
 exports.should_Get_200_On_Get = function (done) {
   supertest(app).get('/api/users/99/activeStreams').expect(200).end(done);
 };
+
+exports.should_Get_201_On_Post = function (done) {
+  supertest(app).post('/api/users/99/activeStreams').expect(201).end(done);
+};
+
+exports.should_Get_404_If_Route_Invalid = function (done) {
+  supertest(app).post('/api/users/99/kljshdfkjsd').expect(404).end(done);
+};
--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const app = http.createServer((req, res) => {
     const userId = req.url.split('/')[3];
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Feeds found' }));
+  } else if (
+    req.url.match(/\/api\/users\/([0-9]+)\/activeStreams/) &&
+    req.method === 'POST'
+  ) {
+    const userId = req.url.split('/')[3];
+    res.writeHead(201, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Stream Added' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route Not Found' }));
   }
 });
 
